23ba6ed0a4ef7a3748fa2291a04967e9916194df
Elijah Littell <elittell214@gmail.com>
2021-03-29 12:12:57 -0400

    Added comments

--- react-practice-testing/src/index.js
+++ react-practice-testing/src/index.js
@@ -1,5 +1,8 @@
-// Deleted everthing in the react-practice-testing/src and /public (except for index)
+// Deleted everthing in the react-practice-testing/src and /public (except for index) 
+// Don't have to delete everthing! But we did!
+// Imported react from React
 import React from 'react';
+// Imported react-dom from ReactDOM we need that to update the page virtually
 import ReactDOM from 'react-dom';
 
 const element = <h1 id="hi">Hello World</h1>;
