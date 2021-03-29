6bfcaa3b7e48bd3d007e83bc0ffbd0ed65d05f69
Elijah Littell <elittell214@gmail.com>
2021-03-29 14:14:36 -0400

    Added a method

--- react-practice-testing/src/components/counter.jsx
+++ react-practice-testing/src/components/counter.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    // Any data a component needs
+    state = {
+        count: 0
+    };
     render() {
         return (
             <div>
-                <h1>Hello world</h1>
+                <span>{this.state.count}</span>
                 <button>Increment</button>
             </div>
         );
+    }
+    formatCount() {
+        return this.state.count === 0 ? "Zero" : this.state.count;
     }
 }
 
